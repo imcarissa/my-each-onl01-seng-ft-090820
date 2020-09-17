9dee92ad1922418ea01d57f320e9af2c12ebcc93
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 09:28:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def my_each(array)
   i = 0
 
@@ -6,5 +7,20 @@ def my_each(array)
     i += 1
   end
   
+=======
+def my_each(words)
+  array = []
+  i = 0
+
+  while i < words.length
+    yield(array[i])
+    i = i + 1
+  end
+  
+  my_each(words) do |word|
+      puts word
+    end
+  
+>>>>>>> da2bf02e00042d2a6f27449757409f82259986ed
   array
 end
